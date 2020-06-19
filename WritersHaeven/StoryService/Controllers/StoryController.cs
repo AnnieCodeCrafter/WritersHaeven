e37bbf74d094e8e1dc507713d82a1b25e51f3c11
Anouk <ambrondijk@gmail.com>
2020-06-19 22:59:07 +0200

    added more to story service

---
+++ WritersHaeven/StoryService/Controllers/StoryController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace StoryService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class StoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
